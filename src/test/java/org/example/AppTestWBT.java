2e6d3ba043de6e50dd1890fece194d3493561c45
victortudosa <victor.tudosa@gmail.com>
2020-04-13 19:40:22 +0300

    new tests

--- src/test/java/org/example/AppTestWBT.java
+++ src/test/java/org/example/AppTestWBT.java
@@ -23,7 +23,7 @@ public class AppTestWBT {
 
         int result = service.saveTema("1", "descriere", 7, 6);
 
-        assertEquals(result, 0);
+        assertEquals(result, 1);
     }
 
     @Test
@@ -38,22 +38,139 @@ public class AppTestWBT {
 
         int result = service.saveTema("", "descriere", 7, 6);
 
-        assertEquals(result, 0);
+        assertEquals(result, 1);
     }
 
     @Test
-    public void testtest() {
+    public void test3() {
 
         Validator<Tema> validator = new TemaValidator();
         TemaRepository repo = new TemaRepository(validator);
 
-        //Tema tema = new Tema(1, "descriere", 10, 12);
+        Service service = new Service(null, repo, null);
+
+        int result = service.saveTema("1", "desc", 14, 1);
+
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void test4() {
+
+        Validator<Tema> validator = new TemaValidator();
+        TemaRepository repo = new TemaRepository(validator);
 
         Service service = new Service(null, repo, null);
 
-        int result = service.saveTema("", "descriere", 7, 6);
+        int result = service.saveTema("2", "desc", 7, 7);
+
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void test5() {
+
+        Validator<Tema> validator = new TemaValidator();
+        TemaRepository repo = new TemaRepository(validator);
+
+        Service service = new Service(null, repo, null);
+
+        int result = service.saveTema("1", "desc", 8, 6);
+        result = service.saveTema("1", "desc", 8, 6);
 
         assertEquals(result, 0);
     }
+
+    @Test
+    public void test6() {
+
+        Validator<Tema> validator = new TemaValidator();
+        TemaRepository repo = new TemaRepository(validator);
+
+        Service service = new Service(null, repo, null);
+
+        int result = service.saveTema("", "desc", 8, 6);
+
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void test7() {
+
+        Validator<Tema> validator = new TemaValidator();
+        TemaRepository repo = new TemaRepository(validator);
+
+        Service service = new Service(null, repo, null);
+
+        int result = service.saveTema("1", "", 8, 6);
+
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void test8() {
+
+        Validator<Tema> validator = new TemaValidator();
+        TemaRepository repo = new TemaRepository(validator);
+
+        Service service = new Service(null, repo, null);
+
+        int result = service.saveTema("1", "desc", 0, 6);
+
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void test9() {
+
+        Validator<Tema> validator = new TemaValidator();
+        TemaRepository repo = new TemaRepository(validator);
+
+        Service service = new Service(null, repo, null);
+
+        int result = service.saveTema("1", "desc", 8, 0);
+
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void test10() {
+
+        Validator<Tema> validator = new TemaValidator();
+        TemaRepository repo = new TemaRepository(validator);
+
+        Service service = new Service(null, repo, null);
+
+        int result = service.saveTema("1", "desc", 6, 7);
+
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void test11() {
+
+        Validator<Tema> validator = new TemaValidator();
+        TemaRepository repo = new TemaRepository(validator);
+
+        Service service = new Service(null, repo, null);
+
+        int result = service.saveTema(null, "desc", 6, 7);
+
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void test12() {
+
+        Validator<Tema> validator = new TemaValidator();
+        TemaRepository repo = new TemaRepository(validator);
+
+        Service service = new Service(null, repo, null);
+
+        int result = service.saveTema("1", null, 6, 7);
+
+        assertEquals(result, 1);
+    }
+
 
 }
